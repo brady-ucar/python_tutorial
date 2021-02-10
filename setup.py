d7925bf1e0bb06049febfe7fd856f3917b08f7ee
Esther Brady <brady@ucar.edu>
2021-02-10 13:46:48 -0700

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mysci",
+    version="1.0.0",
+    description="a sample package",
+    author="esther",
+    author_email="brady@ucar.edu",
+    packages=["mysci"],
+    install_requires=[])
+    
